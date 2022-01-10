d68489acf4f15df708500e9b19c31122988fc00c
minnyres <40790553+minnyres@users.noreply.github.com>
2022-01-10 10:06:58 +0800

    Update mingw32_cross.sh

--- scripts/mingw32_cross.sh
+++ scripts/mingw32_cross.sh
@@ -2,6 +2,8 @@
 
 # Dependencies: g++ autoconf automake make patch autopoint bison flex gawk texinfo libtool wget
 
+set -e
+
 export TARGET=i686-w64-mingw32
 export BUILDDIR=$HOME/mingw32-cross
 export GCC_VERSION=8.3.0
@@ -22,18 +24,21 @@ wget http://ftp.gnu.org/gnu/mpc/mpc-$MPC_VERSION.tar.gz
 wget https://gcc.gnu.org/pub/gcc/infrastructure/isl-$ISL_VERSION.tar.bz2
 wget https://jaist.dl.sourceforge.net/project/mingw-w64/mingw-w64/mingw-w64-release/mingw-w64-v$MINGW_VERSION.tar.bz2
 
+# binutils
 cd $BUILDDIR/tmp
 tar -xf binutils-$BINUTILS_VERSION.tar.xz
 cd binutils-$BINUTILS_VERSION
 ./configure --prefix=$BUILDDIR --target=$TARGET --with-sysroot=$BUILDDIR --disable-multilib 
 make -j$(nproc) && make install
 
+# mingw-w64 headers
 cd $BUILDDIR/tmp
 tar -xf mingw-w64-v$MINGW_VERSION.tar.bz2
 cd mingw-w64-v$MINGW_VERSION/mingw-w64-headers
 ./configure --prefix=$BUILDDIR/$TARGET --host=$TARGET
 make all && make install
 
+# gcc-core
 cd $BUILDDIR/tmp
 tar -xf gcc-$GCC_VERSION.tar.xz
 cd gcc-$GCC_VERSION
@@ -46,9 +51,6 @@ mv -v mpc-$MPC_VERSION mpc
 tar -xf ../isl-$ISL_VERSION.tar.bz2
 mv -v isl-$ISL_VERSION isl
 
-wget https://downloads.freehamster.org/gcc-fix-mingw-pch.patch
-patch -p0 < gcc-fix-mingw-pch.patch
-
 mkdir build
 cd build
 ../configure --prefix=$BUILDDIR --target=$TARGET --with-sysroot=$BUILDDIR --disable-multilib  --disable-shared --enable-languages=c,c++ --enable-threads=posix --disable-win32-registry --enable-version-specific-runtime-libs --enable-fully-dynamic-string --enable-libgomp --enable-libssp --enable-lto 
@@ -59,22 +61,26 @@ make -j$(nproc) all-gcc && make install-gcc
 
 export PATH=$BUILDDIR/bin:$PATH
 
+# mingw-w64 crt
 cd $BUILDDIR/tmp/mingw-w64-v$MINGW_VERSION/mingw-w64-crt/
 ./configure --prefix=$BUILDDIR/$TARGET --host=$TARGET --enable-lib64=no --enable-lib32 
 make -j$(nproc) && make install
 
+# winpthreads
 cd $BUILDDIR/tmp/mingw-w64-v$MINGW_VERSION/mingw-w64-libraries/winpthreads/
 ./configure --prefix=$BUILDDIR/$TARGET --host=$TARGET --enable-shared=no --enable-static
 make -j$(nproc) && make install
 
+# gcc libs
 cd $BUILDDIR/tmp/gcc-$GCC_VERSION/build
 make -j$(nproc) && make install
 
-
+# clean
 cd $BUILDDIR/
 rm -rf $BUILDDIR/tmp/
 rm $BUILDDIR/mingw
 
+# strip
 cd $BUILDDIR/
 find -executable -type f -exec strip -s {} ";" >& /dev/null
 find . -name "*.a" -type f -exec $TARGET-strip -d {} ";" >& /dev/null
