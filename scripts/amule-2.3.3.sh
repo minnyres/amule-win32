cdb891c8de48507af88a982ea52dde6b677c798a
minnyres <liuthgw23@gmail.com>
2022-01-11 15:26:17 +0800

    disable ipv6 for upnp

--- scripts/amule-2.3.3.sh
+++ scripts/amule-2.3.3.sh
@@ -89,7 +89,7 @@ cd $BUILDDIR
 wget https://github.com/pupnp/pupnp/releases/download/release-1.14.12/libupnp-1.14.12.tar.bz2
 tar -xf libupnp-1.14.12.tar.bz2
 cd libupnp-1.14.12
-./configure --host=$TARGET --prefix=$BUILDDIR/tmp/libupnp --enable-static=yes --enable-shared=no --disable-samples
+./configure --host=$TARGET --prefix=$BUILDDIR/tmp/libupnp --enable-static=yes --enable-shared=no --disable-samples --disable-ipv6
 make install
 sed -i 's/-lupnp -lixml/& -liphlpapi -lws2_32 /g'  $BUILDDIR/tmp/libupnp/lib/pkgconfig/libupnp.pc
 
