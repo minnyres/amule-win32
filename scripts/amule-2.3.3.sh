f353b71ec6efdab077deadef62b490e22d4b2bb6
minnyres <liuthgw23@gmail.com>
2022-01-20 12:24:25 +0800

    make: parallel building, install after building

--- scripts/amule-2.3.3.sh
+++ scripts/amule-2.3.3.sh
@@ -30,7 +30,7 @@ wget http://www.zlib.net/zlib-1.2.11.tar.gz
 tar -xf zlib-1.2.11.tar.gz
 cd zlib-1.2.11/
 CC=$TARGET-gcc AR="$TARGET-ar" RANLIB=$TARGET-ranlib ./configure --prefix=$BUILDDIR/tmp/zlib --static
-make install
+make -j$(nproc) && make install
 
 # cryptopp
 cd $BUILDDIR
@@ -38,7 +38,7 @@ wget http://cryptopp.com/cryptopp860.zip
 mkdir cryptopp
 cd cryptopp
 7z x ../cryptopp860.zip
-CXX=$TARGET-g++ RANLIB=$TARGET-ranlib AR=$TARGET-ar LDLIBS=-lws2_32 make -f GNUmakefile
+CXX=$TARGET-g++ RANLIB=$TARGET-ranlib AR=$TARGET-ar LDLIBS=-lws2_32 make -f GNUmakefile -j$(nproc)
 PREFIX=$BUILDDIR/tmp/cryptopp make -f GNUmakefile install
 
 # libiconv
@@ -49,7 +49,7 @@ cd libiconv-1.16
 ./configure --host=$TARGET --prefix=$BUILDDIR/tmp/libiconv --enable-static=yes --enable-shared=no
 mkdir -p $BUILDDIR/tmp/libiconv/lib
 ln -s lib $BUILDDIR/tmp/libiconv/lib64
-make install
+make -j$(nproc) && make install
 
 # gettext
 cd $BUILDDIR
@@ -59,7 +59,7 @@ cd gettext-0.21
 ./configure CPPFLAGS="-I$BUILDDIR/tmp/libiconv/include"  LDFLAGS="-L$BUILDDIR/tmp/libiconv/lib" --host=$TARGET --prefix=$BUILDDIR/tmp/gettext --with-libiconv-prefix=$BUILDDIR/tmp/libiconv --enable-shared=no --enable-static=yes --enable-threads=posix
 mkdir -p $BUILDDIR/tmp/gettext/lib
 ln -s lib $BUILDDIR/tmp/gettext/lib64
-make install
+make -j$(nproc) && make install
 
 # boost
 cd $BUILDDIR
@@ -84,7 +84,7 @@ cd wxWidgets-3.0.5
 ./configure CPPFLAGS="-I$BUILDDIR/tmp/zlib/include" LDFLAGS="-L$BUILDDIR/tmp/zlib/lib"  --host=$TARGET --prefix=$BUILDDIR/tmp/wxwidgets --with-zlib=sys --with-msw --with-libiconv-prefix=$BUILDDIR/tmp/libiconv --disable-shared --disable-debug_flag --disable-mediactrl --enable-optimise --enable-unicode 
 mkdir -p $BUILDDIR/tmp/wxwidgets/lib
 ln -s lib $BUILDDIR/tmp/wxwidgets/lib64
-make install
+make -j$(nproc) && make install
 
 # geoip
 cd $BUILDDIR
@@ -94,7 +94,7 @@ cd geoip-api-c
 ./configure --host=$TARGET --prefix=$BUILDDIR/tmp/geoip --enable-shared=no
 mkdir -p $BUILDDIR/tmp/geoip/lib
 ln -s lib $BUILDDIR/tmp/geoip/lib64
-make install
+make -j$(nproc) && make install
 
 # libpng
 cd $BUILDDIR
@@ -104,7 +104,7 @@ cd libpng-1.6.37
 ./configure CPPFLAGS="-I$BUILDDIR/tmp/zlib/include" CFLAGS="-I$BUILDDIR/tmp/zlib/include" LDFLAGS="-L$BUILDDIR/tmp/zlib/lib" --host=$TARGET --prefix=$BUILDDIR/tmp/libpng --with-zlib-prefix=$BUILDDIR/tmp/zlib --enable-shared=no 
 mkdir -p $BUILDDIR/tmp/libpng/lib
 ln -s lib $BUILDDIR/tmp/libpng/lib64
-make install
+make -j$(nproc) && make install
 sed -i 's/libs="-lpng16"/libs="-lpng16 -lz"/g'  $BUILDDIR/tmp/libpng/bin/libpng-config
 
 # libupnp
@@ -115,7 +115,7 @@ cd libupnp-1.14.12
 ./configure --host=$TARGET --prefix=$BUILDDIR/tmp/libupnp --enable-static=yes --enable-shared=no --disable-samples --disable-ipv6
 mkdir -p $BUILDDIR/tmp/libupnp/lib
 ln -s lib $BUILDDIR/tmp/libupnp/lib64
-make install
+make -j$(nproc) && make install
 sed -i 's/-lupnp -lixml/& -liphlpapi -lws2_32 /g'  $BUILDDIR/tmp/libupnp/lib/pkgconfig/libupnp.pc
 
 # aMule
@@ -131,7 +131,8 @@ patch -p1 < ../../patches/amule-fix-unzip.patch
 
 ./autogen.sh
 ./configure CPPFLAGS="-I$BUILDDIR/tmp/zlib/include -I$BUILDDIR/tmp/libpng/include" LDFLAGS="-L$BUILDDIR/tmp/zlib/lib -L$BUILDDIR/tmp/libpng/lib"  --prefix=$BUILDDIR/pkg --host=$TARGET --enable-amule-daemon --enable-webserver --enable-amulecmd --enable-amule-gui --enable-cas --enable-wxcas --enable-alc --enable-alcc --enable-fileview --enable-static --enable-geoip --disable-debug --enable-optimize --enable-mmap --with-zlib=$BUILDDIR/tmp/zlib  --with-wx-prefix=$BUILDDIR/tmp/wxwidgets --with-wx-config=$BUILDDIR/tmp/wxwidgets/bin/wx-config --with-crypto-prefix=$BUILDDIR/tmp/cryptopp --with-libintl-prefix=$BUILDDIR/tmp/gettext --with-libiconv-prefix=$BUILDDIR/tmp/libiconv --with-geoip-static -with-geoip-lib=$BUILDDIR/tmp/geoip/lib --with-geoip-headers=$BUILDDIR/tmp/geoip/include  --with-libpng-prefix=$BUILDDIR/tmp/libpng --with-libpng-config=$BUILDDIR/tmp/libpng/bin/libpng-config --enable-static-boost --with-boost=$BUILDDIR/tmp/boost --with-libupnp-prefix=$BUILDDIR/tmp/libupnp --enable-ccache --with-denoise-level=0 >configure.log.txt 2>&1
-make install BOOST_SYSTEM_LIBS="$BUILDDIR/tmp/boost/lib/libboost_system.a -lws2_32" 
+make BOOST_SYSTEM_LIBS="$BUILDDIR/tmp/boost/lib/libboost_system.a -lws2_32" -j$(nproc)
+make install
 
 # strip and archive 
 $TARGET-strip $BUILDDIR/pkg/bin/*
