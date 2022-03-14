17ed82e1f3ed2472fb6dc37aff4a8490081d3a05
minnyres <liuthgw23@gmail.com>
2022-03-14 12:37:21 +0800

    update llvm-mingw build script

--- scripts/llvm-mingw.sh
+++ scripts/llvm-mingw.sh
@@ -2,10 +2,12 @@
 
 set -e
 
+git clone https://github.com/mstorsjo/llvm-mingw.git
 cd llvm-mingw
-
 sed  -i "s/DEFAULT_MSVCRT:=ucrt/DEFAULT_MSVCRT:=msvcrt/"  build-mingw-w64.sh
 
 ./build-all.sh ../toolchain/clang
+./strip-llvm.sh ../toolchain/clang
 
-git restore .
+cd ..
+rm -rf llvm-mingw
