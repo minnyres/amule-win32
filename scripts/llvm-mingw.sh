1b293dbdb489700efaa62cd9cb9ef8d79a6b35d5
minnyres <liuthgw23@gmail.com>
2022-03-19 19:59:10 +0800

    switch to llvm 13.0 stable

--- scripts/llvm-mingw.sh
+++ scripts/llvm-mingw.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-git clone https://github.com/mstorsjo/llvm-mingw.git
+git clone --depth 1 --branch 20211002  https://github.com/mstorsjo/llvm-mingw.git
 cd llvm-mingw
 sed  -i "s/DEFAULT_MSVCRT:=ucrt/DEFAULT_MSVCRT:=msvcrt/"  build-mingw-w64.sh
 
