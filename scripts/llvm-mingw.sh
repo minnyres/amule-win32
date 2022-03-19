b4c059c90d9abf41a7fee0ac6728dd7a7fd5197b
minnyres <liuthgw23@gmail.com>
2022-03-19 20:59:30 +0800

    add option to choose the crt

--- scripts/llvm-mingw.sh
+++ scripts/llvm-mingw.sh
@@ -2,9 +2,29 @@
 
 set -e
 
+help_msg="Usage: ./scripts/llvm-mingw.sh -crt=[ucrt/msvcrt]"
+
+if [ $# == 1 ]
+then
+    if [ $1 == "-crt=ucrt" ]
+    then
+        CRT=ucrt
+    elif [ $1 == "-crt=msvcrt" ]
+    then
+        CRT=msvcrt
+    else 
+        echo $help_msg
+        exit -1
+    fi 
+else
+    echo $help_msg
+    exit -1
+fi
+
 git clone --depth 1 --branch 20211002  https://github.com/mstorsjo/llvm-mingw.git
 cd llvm-mingw
-sed  -i "s/DEFAULT_MSVCRT:=ucrt/DEFAULT_MSVCRT:=msvcrt/"  build-mingw-w64.sh
+
+sed  -i "s/DEFAULT_MSVCRT:=ucrt/DEFAULT_MSVCRT:=$CRT/"  build-mingw-w64.sh
 
 ./build-all.sh ../toolchain/clang
 ./strip-llvm.sh ../toolchain/clang
