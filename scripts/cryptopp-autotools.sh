527fed4bd78a0bd1a709df1a36a71ebbc7afef31
minnyres <liuthgw23@gmail.com>
2022-05-28 22:25:37 +0800

    cryptopp needs to be built in the root source directory

--- scripts/cryptopp-autotools.sh
+++ scripts/cryptopp-autotools.sh
@@ -10,10 +10,7 @@ autoupdate
 libtoolize --force --install
 autoreconf --force --install
 
-mkdir build-$TARGET
-cd build-$TARGET
-
-CPPFLAGS="-DNDEBUG" CXXFLAGS="-g0 -O3" LDFLAGS="-s" ../configure --prefix=$BUILDDIR/cryptopp --host=$TARGET --enable-shared=no --enable-static
+CPPFLAGS="-DNDEBUG" CXXFLAGS="-g0 -O3" LDFLAGS="-s" ./configure --prefix=$BUILDDIR/cryptopp --host=$TARGET --enable-shared=no --enable-static
 mkdir -p $BUILDDIR/cryptopp/lib
 ln -s lib $BUILDDIR/cryptopp/lib64
 make -j$(nproc)
